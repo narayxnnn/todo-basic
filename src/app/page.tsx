ee5104b0dac68ec41884da77f8b4aac3e6f228f9
Your Name <you@example.com>
2024-11-26 11:36:41 +0530

    UI changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,102 @@
 "use client";
 import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 type todo = {
   title: string;
-  description: string;
-  date: Date;
+  status: string;
+  id: string;
 };
 export default function Home() {
   const [todo, setTodo] = useState<todo[]>([]);
   const [title, setTitle] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
+  //const [status, setStatus] = useState<string>("Pending");
+
   const addTodo = () => {
     const newTodo = {
       title: title,
-      description: description,
-      date: new Date(),
+      status: "Pending",
+      id: title,
     };
     setTodo([...todo, newTodo]);
     setTitle("");
-    setDescription("");
+  };
+
+  const handleStatus = (idd) => {
+    const currentItemIndex = todo.findIndex((t) => t.id == idd);
+    const updatedTodo = [...todo];
+    updatedTodo[currentItemIndex] = {
+      ...updatedTodo[currentItemIndex],
+      status: "Completed",
+    };
+    setTodo(updatedTodo);
+  };
+
+  const closeTodo = (idd) => {
+    const currentItemIndex = todo.findIndex((t) => t.id == idd);
+    const afterDeletedTodo = [
+      ...todo.slice(0, currentItemIndex),
+      ...todo.slice(currentItemIndex + 1),
+    ];
+    setTodo(afterDeletedTodo);
   };
   return (
-    <div>
-      <div className="flex my-16 items-center justify-center gap-6">
+    <div className="mx-8 my-8 mt-8 mb-8 bg-purple-50 pb-6 rounded-lg">
+      <p className=" flex justify-center mx-auto rounded-t-lg bg-purple-400 pt-6 font-semibold text-white text-3xl pb-24">
+        TODO List
+      </p>
+
+      <div className="flex rounded-lg -mt-16 sm:mx-8 shadow-md bg-stone-50 py-8 flex-col items-center mx-4 my-4 mb-4 mt-4 justify-center gap-2">
         <input
           type="text"
-          placeholder="title"
-          className="border"
+          placeholder="What would you like to do?"
+          className="border-b-2 outline-none w-56 text-lg border-gray-400 bg-stone-50"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
-        <input
-          type="text"
-          placeholder="description"
-          className="border"
-          required
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <button className="border rounded" onClick={addTodo}>
+        <button
+          className="border rounded-md hover:bg-red-500 hover:scale-105 transition duration-300 bg-red-400 text-white px-6 py-2 font-semibold shadow-lg shadow-red-200"
+          onClick={addTodo}
+        >
           Add todo
         </button>
       </div>
-      <div className="flex justify-center gap-4">
-        <h1>Todo List: </h1>
-        <table className="border ">
-          <thead className="border">
-            <tr>
-              <td className="border">title</td>
-              <td className="border">description</td>
-              <td className="border">date</td>
-              <td className="border">delete</td>
+
+      <div className="mx-4 my-4 shadow-md sm:mx-8 bg-stone-50 py-6 rounded-lg">
+        <table className="flex flex-col">
+          <thead className="bg-stone-100">
+            <tr className="flex justify-between px-6 py-3">
+              <td className="w-1/2">List</td>
+              <td>Status</td>
+              <td>Close</td>
             </tr>
           </thead>
           {todo.map((todo, index) => (
             <tbody key={index}>
-              <tr key={index}>
-                <td className="border">{todo.title}</td>
-                <td className="border">{todo.description}</td>
-                <td className="border">{todo.date.toISOString()}</td>
-                <td className="border hover:bg-gray-400 rounded-full">
-                  <button>delete</button>
+              <tr
+                key={index}
+                className="flex justify-between px-6 shadow-sm py-5"
+              >
+                <td className="w-1/2 overflow-x-scroll">{todo.title}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      handleStatus(todo.id);
+                    }}
+                    className="flex items-center justify-between bg-teal-600 hover:bg-teal-700 hover:scale-105 transition duration-300 font-semibold text-white px-2 py-1 rounded-md"
+                  >
+                    {todo.status}
+                  </button>
+                </td>
+                <td>
+                  <button
+                    onClick={() => {
+                      closeTodo(todo.id);
+                    }}
+                    className="text-red-500 hover:text-red-600 hover:scale-105 transition duration-300 shadow-lg"
+                  >
+                    <FaTrashAlt />
+                  </button>
                 </td>
               </tr>
             </tbody>
